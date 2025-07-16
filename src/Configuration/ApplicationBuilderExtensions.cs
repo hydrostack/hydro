58fc29dcddf40b81784f5649b8e20f2ae12d132a
R3dByt3 <marvin.gerdel@web.de>
2025-07-16 11:28:25 +0200

    Update ApplicationBuilderExtensions.cs

--- src/Configuration/ApplicationBuilderExtensions.cs
+++ src/Configuration/ApplicationBuilderExtensions.cs
@@ -1,9 +1,7 @@
-﻿using Hydro.Utils;
-using Newtonsoft.Json;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.FileProviders;
-using Microsoft.AspNetCore.Hosting;
 using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.FileProviders;
 
 namespace Hydro.Configuration;
 
@@ -12,19 +10,13 @@ namespace Hydro.Configuration;
 /// </summary>
 public static class ApplicationBuilderExtensions
 {
-    private static readonly JsonSerializerSettings JsonSerializerSettings = new()
-    {
-        Converters = new JsonConverter[] { new Int32Converter() }.ToList()
-    };
-
     /// <summary>
     /// Adds configuration for hydro
     /// </summary>
     /// <param name="builder">The <see cref="IApplicationBuilder"/> instance this method extends.</param>
     /// <param name="environment">Current environment</param>
-    /// <param name="serializerSettingsMutator">Modification callback for custom serialization, in case of polymorphism within components</param>
-    public static IApplicationBuilder UseHydro(this IApplicationBuilder builder, IWebHostEnvironment environment = null, Action<JsonSerializerSettings> serializerSettingsMutator = null) =>
-        builder.UseHydro(environment, Assembly.GetCallingAssembly(), serializerSettingsMutator);
+    public static IApplicationBuilder UseHydro(this IApplicationBuilder builder, IWebHostEnvironment environment = null) =>
+        builder.UseHydro(environment, Assembly.GetCallingAssembly());
 
     /// <summary>
     /// Adds configuration for hydro
@@ -32,21 +24,16 @@ public static class ApplicationBuilderExtensions
     /// <param name="builder">The <see cref="IApplicationBuilder"/> instance this method extends.</param>
     /// <param name="environment">Current environment</param>
     /// <param name="assembly">Assembly to scan for the Hydro components</param>
-    /// <param name="serializerSettingsMutator">Modification callback for custom serialization, in case of polymorphism within components</param>
     /// <returns></returns>
-    public static IApplicationBuilder UseHydro(this IApplicationBuilder builder, IWebHostEnvironment environment, Assembly assembly, Action<JsonSerializerSettings> serializerSettingsMutator = null)
+    public static IApplicationBuilder UseHydro(this IApplicationBuilder builder, IWebHostEnvironment environment, Assembly assembly)
     {
-        var settings = JsonSerializerSettings;
-        
-        serializerSettingsMutator?.Invoke(settings);
-        
         builder.UseEndpoints(endpoints =>
         {
             var types = assembly.GetTypes().Where(t => t.IsAssignableTo(typeof(HydroComponent))).ToList();
 
             foreach (var type in types)
             {
-                endpoints.MapHydroComponent(type, settings);
+                endpoints.MapHydroComponent(type);
             }
         });
 
